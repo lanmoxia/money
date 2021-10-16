b4c80cba5045aeac5117de395c6ba9c43eefbcd4
LiYongFeng <506076395@qq.com>
2021-10-16 21:33:48 +0800

    CSS 重复布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-  Labels.vue
-    <Nav/>
+  <div class="nav-wrapper">
+    <div class="content">
+    <span>Labels.vue</span>
+  </div>
+  <Nav/>
   </div>
 </template>
 
@@ -12,5 +14,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  // 最小宽度不行 就固定宽度
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  border: 1px solid red;
+  flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,8 +1,10 @@
 <template>
-<div>
-  Statistics.vue
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      <span>Statistics.vue</span>
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,5 +14,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  // 最小宽度不行 就固定宽度
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  border: 1px solid red;
+  flex-grow: 1;
+}
 </style>
