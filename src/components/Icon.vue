d5c52d333c72697774a67d3bd4f1aeac10e287df
LiYongFeng <506076395@qq.com>
2021-10-18 01:50:30 +0800

    引入 SVG

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg class="icon">
+      <use v-bind:xlink:href= "'#'+ name"/>
+    </svg>
+</template>
+
+<script lang="ts">
+//下边代码可实现将任意目录后缀的文件统一全部引入到当前文件
+//定义一个 importAll 的方法
+// 接受一个对象，调用它的 keys 然后遍历
+// 因为这里是 ts 所以要告诉 ts 这个对象的类型
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+//单元测试的时候可能会报错，这里 try 一下 不要中断我的代码
+try {
+  // 这样就可以使用 importAll  然后指定去哪个目录
+  importAll(require.context('../assets/icons', true, /\.svg$/))
+}catch(error){
+  error.log(error)
+}
+export default {
+  props: ['name'],
+  name: 'Icon'
+};
+</script>
+
+<style lang="scss" scoped>
+  .icon{
+    width: 1em;
+    height: 1em;
+  }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config =>{
+    // dir 目录的路径
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module //Vue 把 webpack 封装暴露出一个 对象 config
+      .rule('svg-sprite') // 添加规则 规则名字
+      .test(/\.svg$/) //规则特点 能匹配这里的正则就能使用
+        .include.add(dir).end() // 只需要 dir 这个目录走这个规则
+        // 配置 loader
+        .use('svg-sprite-loader-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      //配置插件
+      config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+      config.module.rule('svg').exclude.add(dir) // 其他 SVG loader 排除 icons 目录
+  }
 }
