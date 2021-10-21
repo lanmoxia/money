4d15cd28c0cd1611f3d48571029b3ef6bbab800e
LiYongFeng <506076395@qq.com>
2021-10-22 01:31:45 +0800

    NumberPad: .逻辑

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="numberPad">
-    <div class="output">{{ output }}</div>
+    <div class="output">{{ output || 0 }}</div>
     <div class="buttons">
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
@@ -33,22 +33,34 @@ export default class NumberPad extends Vue {
     const button = (event.target as HTMLButtonElement);
     // 这里不强制指定类型 indexOf(input) input 有可能为空 解决警告
     const input = button.textContent as string; // const input = button.textContent!; 这样也可以
+    if (this.output.length === 16) {
+      return;
+    }
     if (this.output === '0') {
       if ('0123456789'.indexOf(input) >= 0) {
         this.output = input;
       } else {
         this.output += input;
       }
-    } else {
-      this.output += input;
+      return;
     }
+    if (this.output.indexOf('.') >= 0) {
+      if (input === '.') {
+        return;
+      }
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
+    }
+    this.output += input;
   }
-};
+  }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/help.scss";
-.numberPad{
+
+.numberPad {
   .output {
     @extend %clearfix;
     font-size: 36px;
@@ -60,41 +72,53 @@ export default class NumberPad extends Vue {
     @extend %innerShadow;
     height: 72px;
   }
-  .buttons{
-  @extend %clearfix;
-    > button{
+
+  .buttons {
+    @extend %clearfix;
+
+    > button {
       width: 25%;
       height: 64px;
       float: left;
       background: transparent;
       border: none;
-      &.ok{
+
+      &.ok {
         height: 64*2px;
         float: right;
       }
-      &.zero{
+
+      &.zero {
         width: 25*2%;
       }
+
       $bg: #f2f2f2;
-      &:nth-child(1){
+
+      &:nth-child(1) {
         background: $bg;
       }
-      &:nth-child(2),&:nth-child(5){
+
+      &:nth-child(2), &:nth-child(5) {
         background: darken($bg, 4%);
       }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
         background: darken($bg, 4*2%);
       }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
         background: darken($bg, 4*3%);
       }
-      &:nth-child(8),&:nth-child(11),&:nth-child(13){
+
+      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
         background: darken($bg, 4*4%);
       }
-      &:nth-child(14){
+
+      &:nth-child(14) {
         background: darken($bg, 4*5%);
       }
-      &:nth-child(12){
+
+      &:nth-child(12) {
         background: darken($bg, 4*6%);
       }
     }
