2a655c50a43e859652fedf0c3b0e95f482262498
LiYongFeng <506076395@qq.com>
2021-10-22 17:33:31 +0800

    Tags: 选中/取消功能完成

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -4,31 +4,53 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <!--      :class="selectedTags.indexOf(tag) >= 0 && 'selected'"-->
+      <!--      可以优化为一个对象：:class="{selected: selectedTags.indexOf(tag)>=0}"-->
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click=" toggle(tag)">
+        {{ tag }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue {
+  // 这里的 dataSource 是外部传进来 尽量不修改 使用 undefined 表示有可能没给值
+  // string[] 这是表示字符串数组
+  // 这里的 @Prop 修饰器在这个类里边 即使没有参数也需要加括号 外部 @Component 不用加
+  @Prop() dataSource: string[] | undefined;// 默认标签数据
+  selectedTags: string[] = [];// 选中的标签
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.tags{
+.tags {
   font-size: 14px;
   padding: 16px;
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
-  > .current{
+
+  > .current {
     display: flex;
     flex-wrap: wrap;
-    > li{
+
+    > li {
+      $bg: #d9d9d9;
       background: #cac9ce;
       //$h: 24px;
       height: 24px;
@@ -39,6 +61,10 @@ export default {
       //確定裡邊只有一行字用 line-height
       //不能確定是一行字 每個 li flex 上下居中
       line-height: 24px;
+
+      &.selected {
+        background: darken($bg, 50%);
+      }
     }
   }
   > .new{
