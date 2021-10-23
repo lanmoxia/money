7acea650ad443dee47d36982b9279993d69a3f5a
LiYongFeng <506076395@qq.com>
2021-10-23 21:02:03 +0800

    Notes :v-model :value 和 @input 绑定 正确版

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,41 @@
 <template>
-  <Layout class-prefix="layout" >
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
+  <Layout class-prefix="layout">
+    <NumberPad @update: value="onUpdateAmount"/>
+    <!--    收入支出只能选一个 这里就不用加 s 了-->
+    <Types @update: value="onUpdateType"/>
+    <Notes @update: value="onUpdateNotes"/>
+    <!--    触发 @update:value 事件 执行 onUpdateTags 函数-->
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
-<script> // 这里暂时使用 JS 定义组件
+<script lang="ts"> // 改成 TS 组件
+import Vue from 'vue';
 import Tags from '@/components/money/Tags.vue';
 import Notes from '@/components/money/Notes.vue';
 import Types from '@/components/money/Types.vue';
 import NumberPad from '@/components/money/NumberPad.vue';
+import {Component} from 'vue-property-decorator';
 
-export default {
-  name: 'Money',
-  components: {Tags, Notes, Types, NumberPad},
-  data() {
-    return {
-      tags: ['衣', '食', '住', '行']
-    };
+@Component({components: {Tags, Notes, Types, NumberPad}})
+export default class Money extends Vue {
+  tags = ['衣', '食', '住', '行'];
+
+  onUpdateTags(value: string[]) { // 这里的参数名随便取
+    // Vue 会把 Tags 组件中 value 标签选中取消状态作为第一个参数传到 onUpdateTags 函数
+    console.log(value);
+  }
+
+  onUpdateNotes(value: string) {
+    console.log(value);
+  }
+
+  onUpdateType(value: string) {
+    console.log(value);
+  }
+
+  onUpdateAmount(value: string) { // 用户输入了 1. 还是一个字符串 所以这里类型不能是 number 只能是 string
+    console.log(value);
   }
 };
 </script>
