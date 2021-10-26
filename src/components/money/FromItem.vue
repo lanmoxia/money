e42938d7e22c3c2fb4653e41dd45b12eb16e93b1
LiYongFeng <506076395@qq.com>
2021-10-26 15:35:02 +0800

    Value 变更为 @Prop && v-model 会赋值用在这里不合适

--- src/components/money/FromItem.vue
+++ src/components/money/FromItem.vue
@@ -2,8 +2,18 @@
   <div>
     <label class="fromItem">
       <span class="name">{{ fieldName }}</span>
+      <!--      由于下边的 value 改成了 @Prop 是不能赋值的 -->
+      <!--      这里的 v-model: :value="value" @change="value = $event.target.value" 相当于赋值了-->
+      <!--      <input type="text"-->
+      <!--             v-model="value"-->
+      <!--             :placeholder="this.placeholder">-->
+
+      <!--      改成下边这样就不是赋值了 通知外部要变更 value-->
+      <!--      onNoteChange函数通过回传到这个组件-->
+      <!--      @input 事件触发得到函数回传更新的 value-->
       <input type="text"
-             v-model="value"
+             :value="value"
+             @input="onNoteChange($event.target.value)"
              :placeholder="this.placeholder">
     </label>
   </div>
@@ -15,7 +25,7 @@ import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class FromItem extends Vue {
-  value = '';
+  @Prop({default: ''}) readonly value!: string; //这里把 value 变成 @Prop 外部传入
 
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;// 问号是有可能不存在
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
   <Layout class-prefix="layout">
+    {{ record }}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <div class="remark-wrapper">
@@ -7,6 +8,7 @@
     </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
+
 </template>
 
 <script lang="ts">
